f6343a6f344c05525cb98a80813dd8971e43c30d
Frank <pikino84@gmail.com>
2019-08-08 16:00:09 -0500

    stuilos
    se subieron los estilos

--- app/config/config.php
+++ app/config/config.php
@@ -7,7 +7,7 @@ define("NAME","mvc");
 //URL PARA ENTRAR A LA CARPETA APP
 define("RUTAAPP",dirname(dirname(__FILE__)));
 //URL PARA ACCEDER A LA CARPETA PUBLIC
-define("rutapublic","http://localhost/mvc");
+define("RUTAPUBLIC","http://localhost/mvc");
 //EL NOMBRE DE EL SITIO
 define("NOMBREAPP","INSTAPHOTO");
 
--- app/vistas/usuarios/login.php
+++ app/vistas/usuarios/login.php
@@ -2,9 +2,24 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=0.1">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <link rel="stylesheer" href="<?= RUTAPUBLIC; ?>/public/css/materialize.min.css"></link>
+        <link rel="stylesheer" href="<?= RUTAPUBLIC; ?>/public/css/style.css"></link>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <title><?= NOMBREAPP; ?></title>
     </head>
     <body>
-        <h1>Pagina de inicio</h1>
+        <nav class="red">
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo"><?= NOMBREAPP; ?></a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="<?= RUTAPUBLIC; ?>/usuarios/login">ENTRAR</a></li>
+                    <li><a href="<?= RUTAPUBLIC; ?>/usuarios/registro">ENTRAR</a></li>
+                </ul>
+            </div>
+        </nav>
+        <main>
+        </main>
     </body>
 </html>
